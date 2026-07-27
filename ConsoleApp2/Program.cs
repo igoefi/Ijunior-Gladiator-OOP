190d043660b2a3f1000a6fd2b9f3668149f8ff39
igoefi <96096968+igoefi@users.noreply.github.com>
2026-07-27 23:09:29 +0300

    Игорь самый лучший мужчина в мире!

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,129 +1,233 @@
-﻿public static class Program
+﻿using System.Security.Cryptography.X509Certificates;
+
+public static class Program
 {
     public static void Main()
     {
-        List<AnimalBase> animalsList = [
-            new Lion(),
-            new Parrot(),
-            new Turtle(),
-            new Infocigan()
-            ];
-        AviariesFactory aviariesFactory = new AviariesFactory();
+        const int CarsCount = 10;
+        const int CarServiceMoney = 20000;
 
-
-        Zoopark zoopark = new Zoopark(aviariesFactory.Create(animalsList));
-        zoopark.Work();
-    }
-}
-
-public class AnimalFactory
-{
-    public List<AnimalBase> CreateRandomCount(AnimalBase animalType, int randomMin, int randomMax)
-    {
-        var list = new List<AnimalBase>();
-        int count = Utils.Random.Next(randomMin, randomMax);
-
-        for (int i = 0; i < count; i++)
-            list.Add(animalType.Multiply());
-
-        return list;
-    }
-}
-
-public class AviariesFactory
-{
-    private const int MinAnimalsCount = 3;
-    private const int MaxAnimalsCount = 8;
-
-    public List<Aviary> Create(List<AnimalBase> animals)
-    {
-        var animalsFactory = new AnimalFactory();
-        var aviares = new List<Aviary>();
-
-        foreach (var animal in animals)
+        var details = new List<Detail>()
         {
-            aviares.Add(new(animalsFactory.CreateRandomCount(animal, MinAnimalsCount, MaxAnimalsCount)));
-        }
+            new Detail("Подшибник", 20, false),
+            new Detail("Датчик масла", 700, false),
+            new Detail("Бензонасос", 5000, false),
+            new Detail("Коленвал", 3000, false),
+            new Detail("Ремень ГРВ", 1200, false),
+            new Detail("Генератор", 9000, false),
+            new Detail("Аккумулятор", 2500, false),
+            new Detail("Блок управления", 7000, false),
+            new Detail("Блок ABS", 4500, false),
+            new Detail("Дроссельная заслонка", 800, false)
+        };
 
-        return aviares;
+        var cars = new CarsFactory().CreateCars(CarsCount, details);
+        var servise = new CarService(cars, CarServiceMoney, 
+            new DetailsFactory().CreateDetailsForCarService(details));
+        servise.Work();
     }
 }
 
-public class Aviary
+public class CarService
 {
-    private List<AnimalBase> _animals;
+    const string LookStorageChoise = "1";
+    const string RepairChoise = "2";
+    const string DenyRepairChoise = "3";
 
-    public Aviary(List<AnimalBase> animals) =>
-        _animals = animals;
+    const int DenyRepairCost = 500;
 
-    public override string ToString()
+    private List<Car> _cars;
+    private int _money;
+    private List<Detail> _details;
+
+    public CarService(List<Car> cars, int money, List<Detail> details)
     {
-        if (_animals == null || _animals.Count == 0)
-            return $"В этом вальере никого нет";
-
-        var animalRoster = "";
-
-        foreach (var animal in _animals)
-            animalRoster += animal.ToString() + "\n";
-
-        return $"Здесь обитают животные вида: {_animals[0].Name}. Вот их список:\n{animalRoster}";
+        _cars = cars;
+        _money = money;
+        _details = details;
     }
-}
-
-public class Zoopark
-{
-    private List<Aviary> _aviaries;
-
-    public Zoopark(List<Aviary> aviaries) =>
-        _aviaries = aviaries;
 
     public void Work()
     {
-        foreach (var aviary in _aviaries)
-            Console.WriteLine(aviary.ToString());
+        while(_cars.Count > 0)
+        {
+            Console.Clear();
+            var car = _cars.First();
+
+            Console.WriteLine($"Твой автосервис:\nДеньги: {_money}\n" +
+                $"Стоимость отказа от машины:{DenyRepairCost}\nСледующая машина:");
+            Console.WriteLine(car.ToString());
+
+
+            string userChoise = Utils.GetUserInput($"\n{LookStorageChoise}) Посмотреть склад\n" +
+                $"{RepairChoise}) Начать починку машины\n{DenyRepairChoise}) Отказаться от починки\n");
+
+            switch (userChoise)
+            {
+                case LookStorageChoise:
+                    LookStorage();
+                    break;
+
+                case RepairChoise:
+                    Repair();
+                    break;
+
+                case DenyRepairChoise:
+                    DenyRepair();
+                    break;
+            }
+        }
+
+        Console.WriteLine("Машины закончились");
+    }
+
+    private void LookStorage()
+    {
+        Console.Clear();
+
+        foreach (var detail in _details)
+            Console.WriteLine(detail.ToString());
+
+        Console.ReadKey();
+    }
+
+    private void Repair()
+    {
+        Console.Clear();
+
+    }
+
+    private void DenyRepair()
+    {
+        _money -= DenyRepairCost;
+        _cars.Remove(_cars.First());
     }
 }
 
-public class AnimalBase
+public class CarsFactory
+{
+    const int MaxCarRepairReward = 10000;
+
+    public List<Car> CreateCars(int count, List<Detail> details)
+    {
+        var detailsFactory = new DetailsFactory();
+        var cars = new List<Car>();
+
+        for (int i = 0; i < count; i++)
+        {
+            cars.Add(new Car(detailsFactory.CreateDetailsForCars(details), Utils.Random.Next(MaxCarRepairReward)));
+        }
+
+        return cars;
+    }
+}
+
+public class DetailsFactory()
+{
+    const double BrokeChanse = .5;
+    const int MaxDetailsCount = 0;
+    
+    public List<Detail> CreateDetailsForCars(List<Detail> details)
+    {
+        var list = details.ToList();
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            int j = Utils.Random.Next(i + 1);
+            var temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+
+            if (i == 0)
+                list[i] = list[i].Clone(true);
+            else
+                if (Utils.Random.NextDouble() < BrokeChanse)
+                list[i] = list[i].Clone(true);
+        }
+
+        return list;
+    }
+
+    public List<Detail> CreateDetailsForCarService(List<Detail> details)
+    {
+        var list = new List<Detail>();
+
+        foreach(var detail in details)
+        {
+            var count = Utils.Random.Next(MaxDetailsCount);
+
+            for (int i = 0; i < count; i++)
+                list.Add(detail.Clone(false));
+        }
+
+        return list;
+    }
+
+
+}
+
+public class Car
+{
+    private List<Detail> _details;
+
+    public Car(List<Detail> details, int repairReward)
+    {
+        _details = details;
+        RepairReward = repairReward;
+    }
+
+    public int RepairReward { get; private set; }
+
+    public List<Detail> GetBrokenDetails()
+    {
+        var list = new List<Detail>();
+
+        foreach (Detail detail in _details)
+            if (detail.IsBroke)
+                list.Add(detail);
+
+        return list;
+    }
+
+    public override string ToString()
+    {
+        var detailsInfo = "";
+
+        foreach (var detail in _details)
+            detailsInfo += detail.ToString() + "\n";
+
+        return $"Награда - {RepairReward}.\nДетали:\n{detailsInfo}";
+    }
+}
+public class Detail
 {
     private string _name;
-    private string _sex;
-    private string _sound;
+    private int _cost;
+    public bool IsBroke { get; private set; }
 
-    protected AnimalBase(string sound, string name)
+    public Detail(string name, int cost, bool isBroke)
     {
-        _sex = Utils.Random.NextDouble() > 0.5 ? "мужской" : "женский";
-        _sound = sound;
         _name = name;
+        _cost = cost;
+        IsBroke = isBroke;
     }
 
-    public string Name { get => _name; }
+    public void Repair() =>
+        IsBroke = false;
 
-    public override string ToString() =>
-        $"Пол - {_sex}, звук - {_sound}";
+    public override string ToString()
+    {
+        string state = IsBroke ? "сломано" : "нормальное";
+        return $"{_name}, состояние - {state}. Работающая стоит {_cost}";
+    }
 
-    public AnimalBase Multiply() =>
-        new(_sound, _name);
+    public Detail Clone(bool isBroke) =>
+        new Detail(_name, _cost, isBroke);
 }
 
-public class Lion : AnimalBase
+public class Client
 {
-    public Lion() : base("Арррррр", "Лев") { }
-}
-
-public class Parrot : AnimalBase
-{
-    public Parrot() : base("Ку-ку ки-ки", "Попугай") { }
-}
-
-public class Turtle : AnimalBase
-{
-    public Turtle() : base("Кхххх", "Черепаха") { }
-}
-
-public class Infocigan : AnimalBase
-{
-    public Infocigan() : base("Ку-ку-купи курсы", "Инфоцыгане") { }
+    private Car Car { get; set; }
 }
 
 public static class Utils
