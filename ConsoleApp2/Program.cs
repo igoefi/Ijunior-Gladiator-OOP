72bd95d018d14f7fd96a1777cecb143e5318c2b8
igoefi@mail.ru <igoefi@mail.ru>
2026-08-01 10:10:52 +0300

    Update

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,19 +3,23 @@
     public static void Main()
     {
         //Странное название тушенки)
-        List<Soldier> soldiers = new List<Soldier>()
+        List<Soldier> soldiers1 = new List<Soldier>()
         {
             new Soldier("Ефимов Игорь Денисович",  "Меч", "Гвардии рыцарь-полковник", 1005),
             new Soldier("Сакутин Роман Астольфович",  "Слово", "Гвардии рыцарь-генерал доты", 10000),
             new Soldier("Ефимов Денис Игоревич",  "Танк", "Гвардии рыцарь-генерал", 500),
-            new Soldier("Владимир Владимирович Владикавказович",  "Нож", "Гвардии младший рядовой", 2),
-            new Soldier("Ян Асильник Вольфович",  "Автомат", "Гвардии рыцарь-полковник", 30),
+            new Soldier("Бладимир Владимирович Владикавказович",  "Нож", "Гвардии младший рядовой", 2),
+            new Soldier("Бан Асильник Вольфович",  "Автомат", "Гвардии рыцарь-полковник", 30),
         };
+        List<Soldier> soldiers2 = new List<Soldier>();
 
-        Console.WriteLine("\nТолько имена и звания:");
-        var selected = soldiers.Select(soldier => (soldier.Name, soldier.Rank)).ToList(); ;
+        var selectedChar = 'Б';
 
-        foreach (var soldier in selected)
+        Console.WriteLine("\nПеревод:");
+        soldiers2 = soldiers1.Where(soldier => soldier.Name[0] == selectedChar).ToList(); ;
+        soldiers1 = soldiers1.Except(soldiers2).ToList();
+
+        foreach (var soldier in soldiers2)
             Console.WriteLine($"{soldier.Name}, звание {soldier.Rank}");
     }
 }
